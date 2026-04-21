765b1251371f0e1e818fe511a7c6cba33e417f35
Nancia Laudino <nancialaudino@gmail.com>
2026-04-21 18:20:10 +0200

    Add script

---
+++ script.js
@@ -0,0 +1,19 @@
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll("nav a");
+
+window.addEventListener("scroll", () => {
+    let current = "";
+
+    sections.forEach((section) => {
+        if (window.scrollY >= section.offsetTop - 100) {
+            current = section.getAttribute("id");
+        }
+    });
+
+    navLinks.forEach((link) => {
+        link.classList.remove("active");
+        if (link.getAttribute("href") === "#" + current) {
+            link.classList.add("active");
+        }
+    });
+});
