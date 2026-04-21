38afb64448365eadb7e623e8241f91426007f397
Nancia Laudino <nancialaudino@gmail.com>
2026-04-21 18:26:55 +0200

    Smooth scroll on nav click

--- script.js
+++ script.js
@@ -17,3 +17,11 @@ window.addEventListener("scroll", () => {
         }
     });
 });
+
+navLinks.forEach((link) => {
+    link.addEventListener("click", (e) => {
+        e.preventDefault();
+        const target = document.querySelector(link.getAttribute("href"));
+        target.scrollIntoView({ behavior: "smooth" });
+    });
+});
